8cb26bfbf8e84604768777c08afe329295e38938
Pedro <pedro_schunck@outlook.com.br>
2025-01-07 21:25:02 -0300

    atualizando secoes

--- src/containers/Sidebar/styles.ts
+++ src/containers/Sidebar/styles.ts
@@ -3,19 +3,25 @@ import { P } from '../../components/Paragrafo/styles'
 
 export const Descricao = styled(P)`
   margin-top: 24px;
-  margin-botton: 40px;
+  margin-bottom: 40px;
 `
 export const BotaoTema = styled.button`
   border-radius: 12px;
   padding: 8px;
-  color: #eee;
+  color: ${(props) => props.theme.corDeFundo};
   font-size: 10px;
   font-weight: bold;
-  background-color: 282a35;
+  background-color: ${(props) => props.theme.corPrincipal};
   cursor: pointer;
 `
+
 export const SidebarContainer = styled.div`
   position: sticky;
   top: 80px;
   left: 0;
+
+  @media (max-width: 768px) {
+    margin-bottom: 40px;
+    text-align: center;
+  }
 `
--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -6,13 +6,14 @@ const Sobre = () => (
   <section>
     <Titulo fontSize={16}>Sobre mim</Titulo>
     <Paragrafo tipo="principal">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam quasi
-      ducimus officia eum aut reiciendis non repudiandae velit hic voluptate
-      quidem totam, debitis natus dicta illo nulla molestiae enim nesciunt!
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis vero
+      inventore, quaerat necessitatibus repudiandae ex? Eligendi in impedit,
+      illo recusandae distinctio, enim commodi delectus harum tempore suscipit
+      dolor nemo nam?
     </Paragrafo>
     <GithubSecao>
-      <img src="https://github-readme-stats.vercel.app/api?username=PedroGS13876&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
-      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=PedroGS13876&layout=compact&langs_count=7&theme=dracula" />
+      <img src="https://github-readme-stats.vercel.app/api?username=ogiansouza&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=ogiansouza&layout=compact&langs_count=7&theme=dracula" />
     </GithubSecao>
   </section>
 )
