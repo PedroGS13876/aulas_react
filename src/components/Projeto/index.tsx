b8eb255f6acc80febddb5712091a29d15dae2c0f
Pedro <pedro_schunck@outlook.com.br>
2025-01-09 12:56:26 -0300

    atualizando

--- src/components/Projeto/index.tsx
+++ src/components/Projeto/index.tsx
@@ -6,8 +6,8 @@ import { Card, LinkBotao } from './styles'
 const Projeto = () => {
   return (
     <Card>
-      <Titulo>Projeto Lista de tarefas</Titulo>
-      <Paragrafo tipo="secundario">Lista de tarefas feita com VueJS</Paragrafo>
+      <Titulo>Lista de cursos</Titulo>
+      <Paragrafo tipo="secundario">Lista de cursos feita com VueJS</Paragrafo>
       <LinkBotao>Visualizar</LinkBotao>
     </Card>
   )
